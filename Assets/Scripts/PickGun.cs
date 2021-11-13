7b5e11f1fdae605b3cb534a412c9492a9b3d846d
StavRabinovich <stavrab@gmail.com>
2021-11-13 14:28:22 +0200

    Continue

--- Assets/Scripts/PickGun.cs
+++ Assets/Scripts/PickGun.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PickGun : MonoBehaviour
 {
     //public GameObject FloorGun;
     public GameObject GunInHand;
     public GameObject mCamera;
-    public Text gunText;
-    private bool isShown, notTaken, textIsOpen;
+     public TextMeshProUGUI txtGun;
+    private bool notTaken;
+
     // Start is called before the first frame update
     void Start()
     {
+        notTaken = true;
         //FloorGun.gameObject.SetActive(true);
         /*        System.Random rand = new System.Random();
                 isShown = rand.Next(0,2)>0;
@@ -29,27 +32,24 @@ public class PickGun : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        if (Physics.Raycast(mCamera.transform.position, mCamera.transform.forward, out hit))
+        if ((notTaken) && (Physics.Raycast(mCamera.transform.position, mCamera.transform.forward, out hit)))
         {
-            /*if (notTaken && !textIsOpen)
-            {
-                textIsOpen = true;
-                gunText.text = "Press [E] to take gun.";
-            }
-            else if (!notTaken)
-            {
-                textIsOpen = false;
-            }*/
             if (hit.transform.gameObject.tag == "FloorGun") // The view is focused on GunInDrawer
             {
+                txtGun.text = "Press [E] to TAKE the gun";
                 if(Input.GetKeyDown(KeyCode.E))
                 {
                     notTaken = false;
                     hit.transform.gameObject.gameObject.SetActive(false);
                     // FloorGun.gameObject.SetActive(false);
                     GunInHand.gameObject.SetActive(true);
-                }
+                } 
             }
+            else
+            {
+                txtGun.text = "No Gun";
+            } 
         }
+        
     }
 }
